ec7e5c8acd2df86b7c5e20de4195b18b5965ac33
peturik <peturik@gmail.com>
2024-06-15 10:49:50 +0200

    new varsztat app

---
+++ src/app/rekrutacja/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Rekrutacja() {
+  return (
+    <div>
+      <h1>Rekrutacja</h1>
+    </div>
+  );
+}
---
+++ src/app/wsparcia/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Wsparcia() {
+  return (
+    <main className="main container site-about">
+      <div className="main__content">
+        <h2>
+          WARSZTAT TERAPII ZAJĘCIOWEJ
+          <br />
+          Truskawkowa 4<br />
+          PRZEKAZ DAROWIZNĘ
+          <br />
+          <a href="./index.html">19 1090 1665 0000 0001 5004 9851</a>
+        </h2>
+        <img
+          className="wsparczia-img"
+          src="./photo/wsparczia.jpg"
+          alt="wsparczia"
+        />
+      </div>
+    </main>
+  );
+}
