3282e15165525f7195184460893946a50792b810
Artem Petrashyshyn <artem.petrashyshyn@gmail.com>
2024-06-29 13:13:17 +0200

    pracownia_img

--- src/app/pracownie/plastyczna/page.tsx
+++ src/app/pracownie/plastyczna/page.tsx
@@ -3,24 +3,73 @@ import React from "react";
 export default function Plastyczna() {
   //   console.log("first");
   return (
-    <div className="pracowni-container">
-      <div className="pracownia">
-        <p>
-          <b>Pracownia plastyczna</b>: Pracownia plastyczna to miejsce, gdzie
-          uczestnicy rozwijają swoje umiejętności artystyczne i twórcze. Nauka
-          różnych technik malarskich, rysunku, rzeźby, decoupage'u i innych form
-          plastycznych pozwala zdobywać umiejętności w zakresie kompozycji,
-          kolorystyki, precyzji i wyobraźni artystycznej. Pracownia plastyczna w
-          Warsztatach Terapii Zajęciowej daje uczestnikom możliwość
-          rehabilitacji poprzez sztukę. Pracownia plastyczna jest ważna dla
-          rozwoju kreatywności i wyobraźni. Pomaga rozwijać umiejętności i
-          talenty.{" "}
-        </p>
+    <div className="pracownia">
+      <p>
+        <b>Pracownia plastyczna</b>: Pracownia plastyczna to miejsce, gdzie
+        uczestnicy rozwijają swoje umiejętności artystyczne i twórcze. Nauka
+        różnych technik malarskich, rysunku, rzeźby, decoupage'u i innych form
+        plastycznych pozwala zdobywać umiejętności w zakresie kompozycji,
+        kolorystyki, precyzji i wyobraźni artystycznej. Pracownia plastyczna w
+        Warsztatach Terapii Zajęciowej daje uczestnikom możliwość
+        rehabilitacji poprzez sztukę. Pracownia plastyczna jest ważna dla
+        rozwoju kreatywności i wyobraźni. Pomaga rozwijać umiejętności i
+        talenty.
+      </p>
+      <div className="pracowni__container-img">
         <img
-          className="pracownia__img"
+          className="pracownia__img pracownia__img--left"
           src="/photo/plastyczna.jpg"
           alt="plastyczna.1"
         />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolorum, sed tempore neque eius consequuntur asperiores minima! Consequatur deserunt quos minima nisi id minus dolores tempore. Ex iure ipsam ratione.</p>
+      </div>
+      <div className="pracowni__container-img">
+        <img
+          className="pracownia__img pracownia__img--right"
+          src="/photo/plastyczna.jpg"
+          alt="plastyczna.1"
+        />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolorum, sed tempore neque eius consequuntur asperiores minima! Consequatur deserunt quos minima nisi id minus dolores tempore. Ex iure ipsam ratione.</p>
+      </div>
+      <div className="pracowni__container-img">
+        <img
+          className="pracownia__img pracownia__img--left"
+          src="/photo/plastyczna.jpg"
+          alt="plastyczna.1"
+        />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolorum, sed tempore neque eius consequuntur asperiores minima! Consequatur deserunt quos minima nisi id minus dolores tempore. Ex iure ipsam ratione.</p>
+      </div>
+      <div className="pracowni__container-img">
+        <img
+          className="pracownia__img pracownia__img--right"
+          src="/photo/plastyczna.jpg"
+          alt="plastyczna.1"
+        />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolorum, sed tempore neque eius consequuntur asperiores minima! Consequatur deserunt quos minima nisi id minus dolores tempore. Ex iure ipsam ratione.</p>
+      </div>
+      <div className="pracowni__container-img">
+        <img
+          className="pracownia__img pracownia__img--left"
+          src="/photo/plastyczna.jpg"
+          alt="plastyczna.1"
+        />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolorum, sed tempore neque eius consequuntur asperiores minima! Consequatur deserunt quos minima nisi id minus dolores tempore. Ex iure ipsam ratione.</p>
+      </div>
+      <div className="pracowni__container-img">
+        <img
+          className="pracownia__img pracownia__img--right"
+          src="/photo/plastyczna.jpg"
+          alt="plastyczna.1"
+        />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolorum, sed tempore neque eius consequuntur asperiores minima! Consequatur deserunt quos minima nisi id minus dolores tempore. Ex iure ipsam ratione.</p>
+      </div>
+      <div className="pracowni__container-img">
+        <img
+          className="pracownia__img pracownia__img--left"
+          src="/photo/plastyczna.jpg"
+          alt="plastyczna.1"
+        />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolorum, sed tempore neque eius consequuntur asperiores minima! Consequatur deserunt quos minima nisi id minus dolores tempore. Ex iure ipsam ratione.</p>
       </div>
     </div>
   );
